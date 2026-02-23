2bfd92b071a425b5d6c53e284e2b96341d2de4f0
Ayoub El aissaoui <elaissaouiayoub8918@gmail.com>
2026-02-23 22:54:38 +0000

    done

--- user/accounts/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-# from django.forms.models import user
-
-class login_info(forms.Form):
-    username = forms.CharField(max_length=222, required=True)
-    email = forms.CharField( max_length=222, required=True)
-    password = forms.CharField(widget=forms.PasswordInput())
-
-# class Post(forms.Form):
