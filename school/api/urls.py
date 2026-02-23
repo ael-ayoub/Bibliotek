d83bae8db5edbcb4776566337d868381e8eb6a19
Ayoub El aissaoui <elaissaouiayoub8918@gmail.com>
2026-02-23 22:54:06 +0000

    dome

--- school/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import posts_view, posts_viewPk
-urlpatterns = [
-    path("posts/", posts_view, name="posts_view"),
-    path("posts/<int,pk>", posts_viewPk, name="posts_view"),
-]
