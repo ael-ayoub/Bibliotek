df75f2af07e9b41162170e2009e784a3b5c0fd38
Ayoub El aissaoui <elaissaouiayoub8918@gmail.com>
2026-03-08 00:22:27 +0000

    table was done

--- school/school/settings.py
+++ school/school/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     'livereload',
     "django.contrib.staticfiles",
-    "testing",
+    "relation",
 ]
 
 MIDDLEWARE = [
--- school/school/urls.py
+++ school/school/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from testing import urls
+# from testing import urls
+from relation import urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
